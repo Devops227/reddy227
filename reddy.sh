6df9314f614f97661c2c30ce6d274b5b07169927
Devops227 <reddy.reddy0227@gmail.com>
2025-01-12 21:58:50 -0500

    added my script

--- reddy.sh
+++ reddy.sh
@@ -1,10 +1,54 @@
 #!/bin/bash
 
 
-echo -n ":     "
- 
-read NAME 
+USERID=$(id -u)
 
-echo "$NAME"
+LOGDIR="/var/log"
+LOGNAME="$(echo $0 |awk -F. '{print $1}')
+TIMESTAMP=$(date +%Y -%m -%d -%H -%M -%S)
+LOGFILE="$LOGDIR/$LOGNAME-$TIMESTAMP.log"
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: You must have sudo access to execute this script"
+    exit 1 #other than 0
+fi
+
+VALIDATION (){
+
+        if [ $1 -ne 0 ]
+        then
+           echo -e "$1 ...$R Failure"
+          exit 1
+        else
+          echo -e "$2 ... $G Success"
+        fi
+}
+
+echo "Script is started executing: $TIMESTAMP" &>>$LOGFILE
+
+dnf list installed nodejs &>>$LOGFILE
+
+if [ $? -ne 0 ]
+then
+    dnf install nodejs -y &>>$LOGFILE
+    VALIDATION $? "Installing nodejs"
+else
+    echo -e  "Package is already $G installed"
+fi
+
+dnf list installed nginx &>>$LOGFILE
+
+if [ $? -ne 0 ]
+then
+    dnf install nginx -y &>>$LOGFILE
+    VALIDATION $? "Installing nginx"
+else
+    echo -e "Package is already $G installed"
+fi
 
