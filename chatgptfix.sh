1a93273e587325209ff8860ebe99843f69fc1b8f
Devops227 <reddy.reddy0227@gmail.com>
2025-01-13 14:48:01 -0500

    added my script

---
+++ chatgptfix.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+LOGDIR="/var/log"
+LOGNAME="$(echo $0 |awk -F. '{print $1}')"
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOGFILE="$LOGDIR/$LOGNAME-$TIMESTAMP-backend.log"
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+CHECKROOT (){
+        if [ $USERID -ne 0 ]
+        then
+           echo "ERROR:: You must have sudo access to execute this script"
+           exit 1 # other than 0
+        fi
+}
+
+VALIDATION () {
+    if [ $1 -ne 0 ]
+    then
+       echo -e "$1 ...$R Failure$N"
+       exit 1
+    else
+       echo -e "$2 ... $G Success$N"
+    fi
+}
+
+echo "Script is started executing: $TIMESTAMP" >> "$LOGFILE"
+
+CHECKROOT
+
+dnf module disable nodejs -y &>> "$LOGFILE"
+VALIDATION $? "disabling nodejs"
+dnf module enable nodejs:20 -y &>> "$LOGFILE"
+VALIDATION $? "enabling nodejs:20"
+dnf list installed nodejs &>> "$LOGFILE"
+if [ $? -ne 0 ]
+then
+   dnf install nodejs -y &>> "$LOGFILE"
+   VALIDATION $? "installing nodejs"
+   
+   if ! id "expense" &>/dev/null; then
+      useradd expense
+      VALIDATION $? "adding user"
+   else
+      echo "User 'expense' already exists."
+   fi
+   
+   mkdir /app
+   VALIDATION $? "adding dir"
+   
+   curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>> "$LOGFILE"
+   VALIDATION $? "downloading zip"
+   
+   if [ ! -f /tmp/backend.zip ]; then
+      echo "ERROR:: Download failed. Exiting." >> "$LOGFILE"
+      exit 1
+   fi
+   
+   cd /app
+   unzip /tmp/backend.zip &>> "$LOGFILE"
+   VALIDATION $? "unzipping backend file"
+   
+   npm install &>> "$LOGFILE"
+   VALIDATION $? "installing npm"
+   
+   cp /tmp/malla.sh /etc/systemd/system/backend.service
+   dnf install mysql -y &>> "$LOGFILE"
+   VALIDATION $? "installing mysql"
+   
+   mysql -h databaselocal.reddydevops.store -uroot -p'ExpenseApp@1' < /app/schema/backend.sql
+   VALIDATION $? "adding schema"
+   
+   systemctl daemon-reload &>> "$LOGFILE"
+   VALIDATION $? "reloading backend service"
+   systemctl start backend &>> "$LOGFILE"
+   VALIDATION $? "starting backend"
+   systemctl enable backend &>> "$LOGFILE"
+   VALIDATION $? "enabling backend"
+fi
